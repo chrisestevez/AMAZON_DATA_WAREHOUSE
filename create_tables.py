9d1c6ba4a938a11588b2caa6118c28e28ad7062d
chrisestevez <chrisestevez1@gmail.com>
2021-04-05 17:08:42 -0400

    docs: Documentation update
    - Added print statements to identify script steps

--- create_tables.py
+++ create_tables.py
@@ -4,28 +4,47 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """Drops all tables in database.
+
+    Args:
+        cur (obj): Cursor
+        conn (obj): Connection to database
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """Create all tables for database.
+
+    Args:
+        cur (obj): Cursor
+        conn (obj): Connection to database
+    """    
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """Executes steps to create all table for database
+    """
+    print("Creating tables Process Started")
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['DB'].values()))
     cur = conn.cursor()
 
+    print("Dropping Tables")
     drop_tables(cur, conn)
+    
+    print("Creating Tables")
     create_tables(cur, conn)
 
     conn.close()
+    print("Tables Created")
 
 
 if __name__ == "__main__":
