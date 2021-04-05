e7ccc8906072cc063d9c46c42cfff21fac67ebfc
chrisestevez <chrisestevez1@gmail.com>
2021-04-05 17:09:14 -0400

    docs: Documentation update
    - Added print statements to identify script steps

--- etl.py
+++ etl.py
@@ -4,28 +4,47 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """Load information into staging table
+
+    Args:
+        cur (obj): Cursor
+        conn (obj): Connection
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """Load information into staging table
+
+    Args:
+        cur (obj): Cursor
+        conn (obj): Connection
+    """   
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """Executes script logic. 
+    """
+    print("ETL Process Started")
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['DB'].values()))
     cur = conn.cursor()
     
+    print("Loding Staging Tables")
     load_staging_tables(cur, conn)
+    
+    print("Inserting Data")
     insert_tables(cur, conn)
 
     conn.close()
+    print("ETL Process Completed")
 
 
 if __name__ == "__main__":
